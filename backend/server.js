9f6b745c5f3a57b74c0347e857b43fe98f08e054
Okolie Samuel <okechukwu.okolie.2023@gmail.com>
2026-03-12 11:46:43 +0100

    adding to the codebase

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from 'path'
 
 
 
@@ -15,24 +16,46 @@ dotenv.config();
 
 const port = process.env.PORT || 5001;
 const app = express();
+const __dirname = 
 
-//the database shuld be defined before the routes because the routes will have to source from the database
+//the database should be defined before the routes because the routes will have to source from the database
 
 //middleware
+
+//this allows for the reading of json files
 app.use(express.json());
 // app.use((req,res,next)=>{
 //     console.log('Middleware executed')
 //     next()
 // })
 
+
+//cors is necessay in development because of the different bcend and frtend ports
+if(process.env.NODE_ENV !== 'production'){
+  app.use(cors({
+  origin:'http://localhost:5173/'
+}));
+}
+
+
 //this is the rate limiting middleware
-app.use(cors());
 app.use(rateLimiter);
 
 
 
-
+//routes
 app.use("/api/notes", notesRoutes);
+
+
+//this puts the front and backend in the same domain
+if(process.env.NODE_ENV === 'production'){
+
+  app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+  app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
+  })
+}
 
 //in this setup, the server will only start listening for
 //  requests after a successful connection to the database is established. 
--- frontend/src/components/libs/axios.js
+++ frontend/src/components/libs/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+//in production, theres no local host, so we have to make this dynamic
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5005/api' :'/api'
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5005/api',
+    baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json'
     }
